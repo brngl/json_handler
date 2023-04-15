798ff1cae7aa70f19f098dd2f7bbf638ff275768
Angelo Maiorano <angelomaiorano@Mac-mini.homenet.telecomitalia.it>
2023-04-15 23:02:23 +0200

    chi t'aggia di

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import json
-  
-# Opening JSON file
-f = open('data.json')
-  
-# returns JSON object as 
-# a dictionary
-data = json.load(f)
-  
-# Iterating through the json
-# list
 
-print("\n\n")
-print(data['batters']['batter'][0])
-print("\n\n")
-  
-# Closing file
-f.close()
+def deserialize():  
+    
+    f = open('data.json')
+
+    data = json.load(f)
+
+    print("\n\n")
+    print(data['batters']['batter'][0])
+    print("\n\n")
+    
+    f.close()
+
+def serialize():
+    pass
+
+deserialize()
